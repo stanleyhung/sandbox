88d53ddfe7dfc4c7d2a6566045f3a6b7bb6292b4
stanleyhung <stanley.hung25@berkeley.edu>
2013-10-05 15:29:13 -0700

    implemented a way to wake up my desktop PC from a laptop on the same network

---
+++ wake.py
@@ -0,0 +1,5 @@
+import socket
+#Creates a socket object that uses IP addressing with UDP
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#Sends a magic packet (6 bytes of FF strings followed by PC's MAC address 16 times)
+s.sendto('\xff'*6 + '\x00\x1D\x60\x88\x57\x46'*16, ('192.168.1.255', 80))
