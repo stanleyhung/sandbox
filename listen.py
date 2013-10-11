bcc0ebf163d8048bd307943b0118a1a86a0c09bc
stanleyhung <stanley.hung25@berkeley.edu>
2013-10-11 06:17:17 +0000

    fixed listen

--- listen.py
+++ listen.py
@@ -1,6 +1,10 @@
 import socket;
+import sys;
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('192.168.0.0', 9))
+s.bind(('0.0.0.0', 9))
 while True:
 	print s.recvfrom(65565)
+	sys.stdout.flush()
+	break
+print "DONE\n"
