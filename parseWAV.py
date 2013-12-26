e4e9453f1f57bf4cabb92e3f739c0c31d80ce1a6
stanleyhung <stanley.hung25@berkeley.edu>
2013-12-25 23:27:57 -0800

    parsed data; need to fix EOF inside data

--- parseWAV.py
+++ parseWAV.py
@@ -10,8 +10,9 @@ def error(actual, expected):
 	exit(1)
 
 #Get WAV information from file
-wavName = "10. Horn Of Plenty"
+wavName = "26. I Need You"
 fileName = wavName + ".wav"
+print "Getting WAV metadata from file: " + fileName
 f = open(fileName, "r")
 header = f.read(4)
 if header != 'RIFF':
@@ -43,16 +44,19 @@ dataHeader = f.read(4)
 if dataHeader != 'data':
 	error(dataHeader, 'data')
 dataSize = struct.unpack('I', f.read(4))[0]
+numSamples = (dataSize / numChannels) / bytesPerSample
+print "There are " + str(numSamples) + " samples in the files"
 
 #write WAV metadata into output file
 fileName = wavName + ".csv"
+print "Writing WAV metadata into output file: " + fileName
 f2 = open(fileName, 'w')
 f2.write(wavName + '\n')
 f2.write('numChannels,' + str(numChannels) + ',sampleRate,' + str(sampleRate) +
 	',AvgBytesPerSec,' + str(byteRate) + ',bitsPerSample,' + str(bitsPerSample) +
 	'\n')
-for i in range(1, numChannels):
-	f2.write(str(i) + ',')
+for i in range(0, numChannels):
+	f2.write('channel ' + str(i) + ',')
 f2.write('\n')	
 
 #determine how the data is stored (endianess)
@@ -66,8 +70,21 @@ elif bytesPerSample == 4:
 	storageType = 'i' 
 
 #parse and write sample data
-#for i in range(1, numChannels):
-	#do
-
+print "Beginning to parse data...\n"
+done = False
+while True:
+	for i in range(0, numChannels):
+		data = f.read(bytesPerSample)
+		if data != "":
+			data = struct.unpack(storageType, data)[0]
+			f2.write(str(data) + ',')
+		else:
+			print "length is: " + str(len(data))
+			print "data is: " + data
+			print "type is: " + str(type(data))
+			done = True
+	f2.write('\n')
+	if done == True:
+		break
 f2.close()
-print 'SUCCESS'
+print 'SUCCESS: samples written to output file'
