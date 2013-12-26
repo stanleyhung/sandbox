192ac6ada22b66e49ed5108fb75f67eddd531f21
stanleyhung <stanley.hung25@berkeley.edu>
2013-12-25 22:53:58 -0800

    finished gathering WAV metadata

--- parseWAV.py
+++ parseWAV.py
@@ -9,7 +9,9 @@ def error(actual, expected):
 	print "ERROR, expected: ", expected, " received: ", actual
 	exit(1)
 
-fileName = "10. Horn Of Plenty.wav"
+#Get WAV information from file
+wavName = "10. Horn Of Plenty"
+fileName = wavName + ".wav"
 f = open(fileName, "r")
 header = f.read(4)
 if header != 'RIFF':
@@ -21,9 +23,51 @@ if format != 'WAVE':
 fmtID = f.read(4)
 if fmtID != 'fmt ':
 	error(fmtID, 'fmt ')
-subChunkSize1 = struct.unpack('i', f.read(2))[0]
+subChunkSize1 = struct.unpack('I', f.read(4))[0]
 if subChunkSize1 != 16:
 	error(subChunkSize1, 16)
+audioFormat = struct.unpack('H', f.read(2))[0]
+if audioFormat != 1:
+	error(audioFormat, 1)
+numChannels = struct.unpack('H', f.read(2))[0]
+if numChannels != 1 and numChannels != 2:
+	error(numChannels, 1)
+sampleRate = struct.unpack('I', f.read(4))[0]
+if sampleRate != 8000 and sampleRate != 44100:
+	error(sampleRate, 44100)
+byteRate = struct.unpack('I', f.read(4))[0]
+blockAlign = struct.unpack('H', f.read(2))[0]
+bitsPerSample = struct.unpack('H', f.read(2))[0]
+bytesPerSample = bitsPerSample / 8
+dataHeader = f.read(4)
+if dataHeader != 'data':
+	error(dataHeader, 'data')
+dataSize = struct.unpack('I', f.read(4))[0]
 
+#write WAV metadata into output file
+fileName = wavName + ".csv"
+f2 = open(fileName, 'w')
+f2.write(wavName + '\n')
+f2.write('numChannels,' + str(numChannels) + ',sampleRate,' + str(sampleRate) +
+	',AvgBytesPerSec,' + str(byteRate) + ',bitsPerSample,' + str(bitsPerSample) +
+	'\n')
+for i in range(1, numChannels):
+	f2.write(str(i) + ',')
+f2.write('\n')	
 
+#determine how the data is stored (endianess)
+if bytesPerSample == 1:
+	#unsigned byte
+	storageType = 'B'
+elif bytesPerSample == 2:
+	#signed integer
+	storageType = 'h'
+elif bytesPerSample == 4:
+	storageType = 'i' 
+
+#parse and write sample data
+#for i in range(1, numChannels):
+	#do
+
+f2.close()
 print 'SUCCESS'
