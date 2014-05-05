0ad820afca38a7f2c72f5c1435968e42f17387df
stanleyhung <stanley.hung25@berkeley.edu>
2014-05-04 19:29:28 -0700

    changed testpacketControl to send magic packet

--- testPacketControl.py
+++ testPacketControl.py
@@ -3,4 +3,4 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #Sends a magic packet (6 bytes of FF strings followed by PC's MAC address 16 times)
 s.connect((socket.gethostbyname(socket.gethostname()), 9))
-s.sendall('\xff'*6 + '\x00\x1D\x60\x88\x57\x46'*16)
+s.sendall('Turn On')
